90a08187dbb49b7c5f2051a377c81cad021a0c6c
Andrey <andreyzorin@protonmail.com>
2021-06-11 22:02:07 +0300

    1.2 meals.jsp implementation

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,21 +1,27 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.CALORIES_PER_DAY;
+import static ru.javawebinar.topjava.util.MealsUtil.MEALS;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
-        response.sendRedirect("meals.jsp");
+        log.debug("send to meals");
+
+        request.setAttribute("meals", MealsUtil.filteredByStreams(MEALS, LocalTime.MIN,  LocalTime.MAX, CALORIES_PER_DAY));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,43 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib uri="http://example.com/functions" prefix="f" %>
+
 <html lang="ru">
 <head>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+    </style>
+
     <title>Meals</title>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th></th>
+        <th></th>
+    </tr>
+    <%--@elvariable id="meals" type="ru.javawebinar.topjava.model.MealTo"--%>
+    <c:forEach items="${meals}" var="mealTo">
+        <tr style="color: ${mealTo.excess ? 'red' : 'green'}">
+            <td>${f:formatLocalDateTime(mealTo.dateTime, 'dd.MM.yyyy hh:mm')}</td>
+            <td>${mealTo.description}</td>
+            <td>${mealTo.calories}</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </c:forEach>
+</table>
+
 </body>
 </html>
