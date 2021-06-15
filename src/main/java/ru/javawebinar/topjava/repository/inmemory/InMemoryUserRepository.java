725465e2dc05931a8578dcff311a7e610add4a56
Andrey <andreyzorin@protonmail.com>
2021-06-15 23:34:14 +0300

    1.3 userId in MealRepository implementation

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -21,8 +21,8 @@ public class InMemoryUserRepository implements UserRepository {
     private final Map<Integer, User> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
-    private static final int ADMIN_ID = 1;
-    private static final int USER_ID = 2;
+    public static final int ADMIN_ID = 1;
+    public static final int USER_ID = 2;
 
     @Override
     public boolean delete(int id) {
